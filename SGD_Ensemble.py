f68d41c8dfd4bd958a95e31f5e20056d040ddfed
grantimusPrime <700gtkeller@gmail.com>
2020-12-16 22:03:50 -0700

    Some comments and removal of the submission text file.

--- SGD_Ensemble.py
+++ SGD_Ensemble.py
@@ -2,6 +2,11 @@ from sklearn.base import BaseEstimator
 from sklearn.linear_model import SGDClassifier
 import numpy as np
 
+
+### Produced 78.4% accuracy on MNIST dataset.
+# ten SGD models, one to classify each number.
+# If multiple SGDs in the ensemble return true (multiple believe the number they are seeing is their number), return the first.
+#       Obviously this is a poor way to make decisions.
 class Sgd_ensemble(BaseEstimator):
     def __init__(self):
         self.SGD_Ensemble = []
@@ -15,7 +20,6 @@ class Sgd_ensemble(BaseEstimator):
             sgd_c.fit(X, binary)
             self.SGD_Ensemble.append(sgd_c)
 
-
     def predict(self, X):
         toRet = []
         predictions = []
@@ -28,16 +32,16 @@ class Sgd_ensemble(BaseEstimator):
             if np.sum(row_of_predictions) > 1:
                 multiple_positives += 1
 
-
+            ### if we have no positive predictions, return 0
+            ### if we do have positive predictions, return the first one.
             if np.sum(row_of_predictions) < 1:
                 toRet.append(0)
             else:
-               qq = row_of_predictions.nonzero()[0]
-               toRet.append(qq[0])
+                qq = row_of_predictions.nonzero()[0]
+                toRet.append(qq[0])
 
         print('number of multiple positives', multiple_positives)
         return toRet
-
 
         # for x in X:
         #     highest_confidence = 0
